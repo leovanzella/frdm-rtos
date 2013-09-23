d06b367d260e501ebd6334fdeda757d13b3f81a9
leovanzella <leo.vanzella@hotmail.com>
2013-09-23 12:44:13 -0300

    Atualizado startup para funcionamento do escalonador. Exemplo com xQueue e Blink para led vermelho da placa frdm-kl25z

--- main.c
+++ main.c
@@ -1,40 +1,121 @@
-/*----------------------------------------------------------------------------
- * Name:    Blinky.c
- * Purpose: Freescale MKL25Z128xxx4 LED Flasher
- *----------------------------------------------------------------------------
- * This file is part of the uVision/ARM development tools.
- * This software may only be used under the terms of a valid, current,
- * end user licence from KEIL for a compatible version of KEIL software
- * development tools. Nothing else gives you the right to use this software.
+/*
+    FreeRTOS V7.2.0 - Copyright (C) 2012 Real Time Engineers Ltd.
+
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS tutorial books are available in pdf and paperback.        *
+     *    Complete, revised, and edited pdf reference manuals are also       *
+     *    available.                                                         *
+     *                                                                       *
+     *    Purchasing FreeRTOS documentation will not only help you, by       *
+     *    ensuring you get running as quickly as possible and with an        *
+     *    in-depth knowledge of how to use FreeRTOS, it will also help       *
+     *    the FreeRTOS project to continue with its mission of providing     *
+     *    professional grade, cross platform, de facto standard solutions    *
+     *    for microcontrollers - completely free of charge!                  *
+     *                                                                       *
+     *    >>> See http://www.FreeRTOS.org/Documentation for details. <<<     *
+     *                                                                       *
+     *    Thank you for using FreeRTOS, and thank you for your support!      *
+     *                                                                       *
+    ***************************************************************************
+
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation AND MODIFIED BY the FreeRTOS exception.
+    >>>NOTE<<< The modification to the GPL is included to allow you to
+    distribute a combined work that includes FreeRTOS without being obliged to
+    provide the source code for proprietary components outside of the FreeRTOS
+    kernel.  FreeRTOS is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details. You should have received a copy of the GNU General Public
+    License and the FreeRTOS license exception along with FreeRTOS; if not it
+    can be viewed here: http://www.freertos.org/a00114.html and also obtained
+    by writing to Richard Barry, contact details for whom are available on the
+    FreeRTOS WEB site.
+
+    1 tab == 4 spaces!
+    
+    ***************************************************************************
+     *                                                                       *
+     *    Having a problem?  Start by reading the FAQ "My application does   *
+     *    not run, what could be wrong?                                      *
+     *                                                                       *
+     *    http://www.FreeRTOS.org/FAQHelp.html                               *
+     *                                                                       *
+    ***************************************************************************
+
+    
+    http://www.FreeRTOS.org - Documentation, training, latest information, 
+    license and contact details.
+    
+    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
+    including FreeRTOS+Trace - an indispensable productivity tool.
+
+    Real Time Engineers ltd license FreeRTOS to High Integrity Systems, who sell 
+    the code with commercial support, indemnification, and middleware, under 
+    the OpenRTOS brand: http://www.OpenRTOS.com.  High Integrity Systems also
+    provide a safety engineered and independently SIL3 certified version under 
+    the SafeRTOS brand: http://www.SafeRTOS.com.
+*/
+
+/*
+ * This is a very simple demo that demonstrates task and queue usages only in
+ * a simple and minimal FreeRTOS configuration.  Details of other FreeRTOS 
+ * features (API functions, tracing features, diagnostic hook functions, memory
+ * management, etc.) can be found on the FreeRTOS web site 
+ * (http://www.FreeRTOS.org) and in the FreeRTOS book.
  *
- * This software is supplied "AS IS" without warranties of any kind.
+ * Details of this demo (what it does, how it should behave, etc.) can be found
+ * in the accompanying PDF application note.
  *
- * Copyright (c) 2012 Keil - An ARM Company. All rights reserved.
- *----------------------------------------------------------------------------*/
+*/
+
+/* Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+/* Standard include. */
+#include <stdio.h>
 #include <MKL25Z4.H>
+/* Priorities at which the tasks are created. */
+#define mainQUEUE_RECEIVE_TASK_PRIORITY		( tskIDLE_PRIORITY + 2 )
+#define	mainQUEUE_SEND_TASK_PRIORITY		( tskIDLE_PRIORITY + 1 )
 
+/* The rate at which data is sent to the queue, specified in milliseconds. */
+#define mainQUEUE_SEND_FREQUENCY_MS			( 1000 / portTICK_RATE_MS )
 
-
+/* The number of items the queue can hold.  This is 1 as the receive task
+will remove items as they are added, meaning the send task should always find
+the queue empty. */
+#define mainQUEUE_LENGTH					( 1 )
 
 #define LED_NUM     3                   /* Number of user LEDs                */
 const uint32_t led_mask[] = {1UL << 18, 1UL << 19, 1UL << 1};
 volatile uint32_t msTicks;                            /* counts 1ms timeTicks */
-/*----------------------------------------------------------------------------
-  SysTick_Handler
- *----------------------------------------------------------------------------*/
-void SysTick_Handler(void) {
-  msTicks++;                        /* increment counter necessary in Delay() */
-}
+/*
+ * The tasks as described in the accompanying PDF application note.
+ */
+static void prvQueueReceiveTask( void *pvParameters );
+static void prvQueueSendTask( void *pvParameters );
 
-/*------------------------------------------------------------------------------
-  delays number of tick Systicks (happens every 1 ms)
- *------------------------------------------------------------------------------*/
-__INLINE static void Delay (uint32_t dlyTicks) {
-  uint32_t curTicks;
+/*-----------------------------------------------------------*/
 
-  curTicks = msTicks;
-  while ((msTicks - curTicks) < dlyTicks);
-}
+/* The queue used by both tasks. */
+static xQueueHandle xQueue = NULL;
+
+/* One array position is used for each task created by this demo.  The 
+variables in this array are set and cleared by the trace macros within
+FreeRTOS, and displayed on the logic analyzer window within the Keil IDE -
+the result of which being that the logic analyzer shows which task is
+running when. */
+unsigned long ulTaskNumber[ configEXPECTED_NO_RUNNING_TASKS ];
 
 /*------------------------------------------------------------------------------
   configer LED pins
@@ -64,7 +145,6 @@ __INLINE static void LED_On (uint32_t led) {
   else FPTB->PCOR   = led_mask[led];
 }
 
-
 /*------------------------------------------------------------------------------
   Switch off LEDs
  *------------------------------------------------------------------------------*/
@@ -72,28 +152,88 @@ __INLINE static void LED_Off (uint32_t led) {
   if (led == (LED_NUM-1)) FPTD->PSOR   = led_mask[led];
   else FPTB->PSOR   = led_mask[led];
 }
-/*----------------------------------------------------------------------------
-  MAIN function
- *----------------------------------------------------------------------------*/
-int main (void) {
-  int num     = -1; 
-  int dir     =  1;
 
-  SystemCoreClockUpdate();                      /* Get Core Clock Frequency */
-  SysTick_Config(SystemCoreClock/1000);         /* Generate interrupt each 1 ms    */
-  
-  LED_Config();                             
- 
-  while(1) {
-    /* Calculate 'num': 0,1,...,LED_NUM-1,LED_NUM-1,...,1,0,0,...             */
-    num += dir;
-    if (num == LED_NUM) { dir = -1; num =  LED_NUM-1; } 
-    else if   (num < 0) { dir =  1; num =  0;         }
-    LED_On (num);
-    Delay(500);
-    LED_Off(num);
-    Delay(500);
-  }
-  
+void SystemInit()
+{
+	LED_Config();
 }
 
+unsigned char i,j;
+int main(void)
+{
+	/* Create the queue. */
+	xQueue = xQueueCreate( mainQUEUE_LENGTH, sizeof( unsigned long ) );
+
+	if( xQueue != NULL )
+	{
+		/* Start the two tasks as described in the accompanying application
+		note. */
+		xTaskCreate( prvQueueReceiveTask, ( signed char * ) "Rx", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_RECEIVE_TASK_PRIORITY, NULL );
+		xTaskCreate( prvQueueSendTask, ( signed char * ) "TX", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_SEND_TASK_PRIORITY, NULL );
+
+		/* Start the tasks running. */
+		vTaskStartScheduler();
+	}
+
+	/* If all is well we will never reach here as the scheduler will now be
+	running.  If we do reach here then it is likely that there was insufficient
+	heap available for the idle task to be created. */
+	for( ;; )
+	{		
+	}
+}
+/*-----------------------------------------------------------*/
+
+static void prvQueueSendTask( void *pvParameters )
+{
+portTickType xNextWakeTime;
+const unsigned long ulValueToSend = 100UL;
+	unsigned char i;
+
+	/* Initialise xNextWakeTime - this only needs to be done once. */
+	xNextWakeTime = xTaskGetTickCount();
+
+	for( ;; )
+	{
+		/* Place this task in the blocked state until it is time to run again.
+		The block time is specified in ticks, the constant used converts ticks
+		to ms.  While in the Blocked state this task will not consume any CPU
+		time. */
+		vTaskDelayUntil( &xNextWakeTime, mainQUEUE_SEND_FREQUENCY_MS );
+
+		/* Send to the queue - causing the queue receive task to unblock and
+		print out a message.  0 is used as the block time so the sending 
+		operation will not block - it shouldn't need to block as the queue 
+		should always be empty at this point in the code. */
+		xQueueSend( xQueue, &ulValueToSend, 0 );
+		
+		j = j+2;
+		i = !i;
+		if (i)
+			LED_Off (0);
+		else
+			LED_On(0);
+	}
+}
+/*-----------------------------------------------------------*/
+
+static void prvQueueReceiveTask( void *pvParameters )
+{
+	unsigned long ulReceivedValue;
+
+	for( ;; )
+	{
+		/* Wait until something arrives in the queue - this task will block
+		indefinitely provided INCLUDE_vTaskSuspend is set to 1 in
+		FreeRTOSConfig.h. */
+		xQueueReceive( xQueue, &ulReceivedValue, portMAX_DELAY );
+
+		/*  To get here something must have been received from the queue, but
+		is it the expected value?  If it is, print out a pass message, if no,
+		print out a fail message. */
+		if( ulReceivedValue == 100UL )
+		{
+			i++;
+		}
+	}
+}
